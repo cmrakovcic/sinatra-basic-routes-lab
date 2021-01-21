1f15282acaee11510f49e1e2253fda0473627bad
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-01-21 14:06:27 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Christopher Mrakovcic"
+    end
+
+    get '/hometown' do
+        "My hometown is Franklin Square, New York"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is No Role Modelz by J. Cole"
+    end
 end
